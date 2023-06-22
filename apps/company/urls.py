ed7c065ac647c165255f59e0580196e95ff08fc8
Kudrat <60039616+Kudrat0777@users.noreply.github.com>
2023-06-22 20:31:25 +0500

    страница календаря готова

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -5,4 +5,8 @@ app_name = 'company'
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name='home_page'),
+
+    path('company/update/', views.CompanySettings.as_view(), name='company_update'),
+    path('calendar/', views.Calendar.as_view(), name='calendar'),
+
 ]
--- apps/company/views.py
+++ apps/company/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, UpdateView
+from . import models as company_models
 
 
 class HomePage(TemplateView):
     template_name = 'base.html'
+
+
+class CompanySettings(TemplateView):
+    template_name = 'profile_settings/index.html'
+
+
+class Calendar(TemplateView):
+    template_name = 'calendar/index.html'
